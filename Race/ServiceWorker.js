72ccbbddb3ae28d2844bf9d2469e5919cc693807
Daubster <tsmaks2@gmail.com>
2023-11-14 16:09:31 +0300

    created built race

---
+++ Race/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "DefaultCompany-Endless Race-0.1";
+const contentToCache = [
+    "Build/Race.loader.js",
+    "Build/Race.framework.js",
+    "Build/Race.data",
+    "Build/Race.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
